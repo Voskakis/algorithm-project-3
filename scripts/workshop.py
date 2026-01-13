b0970f9d8ddcfe16b04c9751f4abfde41e772436
Xhosant <thanosmaravel@gmail.com>
2026-01-13 19:50:17 +0200

    More work on workshop.py (future search)

--- scripts/workshop.py
+++ scripts/workshop.py
@@ -10,6 +10,39 @@ from build_pipeline import load_inverted_file
 from enums import EndianType
 from my_types import SearchInput
 
+from collections import defaultdict
+
+def load_tsv_to_map(k: int, threshold: float= 0.01):
+    data = defaultdict(list)
+
+    with open("blast_results.tsv", "r", encoding="utf-8") as f:
+        for line in f:
+            cols = line.rstrip("\n").split("\t")
+
+            # Skip malformed rows
+            if len(cols) < 12:
+                continue
+
+            key = cols[0]     # 1st column
+            col2 = cols[1]    # 2nd column
+            col3 = cols[2]    # 3rd column
+
+            try:
+                col11 = float(cols[10])  # 11th column
+                col12 = float(cols[11])  # 12th column
+            except ValueError:
+                continue
+
+            if col11 < threshold:
+                data[key].append((col2, col3, col12))
+
+    # Sort each list by 12th column (descending) and keep top k
+    for key in data:
+        data[key].sort(key=lambda x: x[2], reverse=True)
+        data[key] = data[key][:k]
+
+    return data
+
 def format_output(image_id: int, results: list[list[int]], r:int=0) -> str:
     header = "Query: {qID}\n"
     bodyPiece = "Nearest neighbor-{count}: {neighborID}\ndistanceApproximate: {dis}\ndistanceTrue: {disT}\n\n"
@@ -67,19 +100,31 @@ def main():
     model.load_state_dict(torch.load(search_input.index_path.name + "/model.pt"))
     model.eval()
 
-    totalAproximateTime = 0
-    totalExhaustTime = 0
-    AFcount = 0
-    recNcount = 0
-
     outputFile = open(search_input.output_file, "a")
     outputFile.write("Neural LSH\n")
 
-    modes = [] #TODO load all modes here by name as string, if ALL then fill manually, always contain BLAST
-    for mode in modes:
-        for q, index in zip(search_input.query_data, range(len(search_input.query_data))):
+    modes = ["BLAST"] #TODO load all modes here by name as string, if ALL then fill manually
+
+    #Handle BLAST work
+    command = ("makeblastdb -in "
+               + search_input.input_data
+               +" -dbtype prot -out swissprot_db\nblastp -db swissprot_db -query "
+               + search_input.query_file
+               +" -outfmt 6 -out blast_results.tsv")
+    start = time.time()
+    subprocess.run(command, shell=True, capture_output=True, text=True)
+    end = time.time()
+    BLAST_time = (end - start)/len(search_input.query_data)
+    BLAST_results = load_tsv_to_map(search_input.nearest_neighbors)
+
+    for q, index in zip(search_input.query_data, range(len(search_input.query_data))):
+        method_results = []
+        method_time = []
+        for mode in modes:
             start = time.time()
-            if (mode=="neural"):
+            if (mode=="BLAST"):
+                continue
+            elif (mode=="neural"):
                 # prediction
                 query_tensor = torch.tensor(q, dtype=torch.float32).unsqueeze(0).view(1, -1)
                 with torch.no_grad():
@@ -104,18 +149,16 @@ def main():
                     # f"{build_input.knn_neighbors + 1}", "-o", "output.txt"] TODO configure properly
                 )
 
-            elif (mode=="BLAST"):
-                BLAST() #TODO blast part
 
             end = time.time()
-            totalAproximateTime += (end - start)
+            method_time.append(end - start)
 
-            # output
-            AFcount += results[0][1]/exhaustResults[0][1]
-            recNcount += sum(x not in results for x in exhaustResults)
+        # output
+        recNcount = sum(x not in method_results[-1] for x in method_results[0])/len(method_results[0])
 
-            outputFile.write(format_output(index, [r1 +r2[1:] for r1, r2 in zip(results, exhaustResults)],search_input.search_radius))
-    outputFile.write(format_footer(len(search_input.query_file), search_input.nearest_neighbors, totalAproximateTime, totalExhaustTime, AFcount, recNcount))
+        #TODO overhaul output
+        outputFile.write(format_output(index, [r1 +r2[1:] for r1, r2 in zip(results, BLAST_results["queryName"])],search_input.search_radius))
+        outputFile.write(format_footer(len(search_input.query_file), search_input.nearest_neighbors, totalAproximateTime, totalExhaustTime, AFcount, recNcount))
     outputFile.close()
 
 if __name__ == "__main__":
